16645525c86de152541a2354edf658a2b73ba868
Ghost <160791341+lGhosty@users.noreply.github.com>
2025-06-18 17:01:51 -0300

    Mudança de nome MySQL

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -5,8 +5,8 @@ dotenv.config();
 export const db = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || 'admin',
-  database: process.env.DB_NAME || 'receitas_db',
+  password: process.env.DB_PASS || 'Admin',
+  database: process.env.DB_NAME || 'BCDMyWebReceitas',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
